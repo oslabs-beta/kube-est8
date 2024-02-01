6ab2fc9d2dc6296d7db6671a9bde318adbd3da5f
ppost0 <p.post2321@yahoo.com>
2024-01-31 19:47:21 -0500

    Initialize React skeleton for app
    Co-authored-by: Ines Chavez <Ineschvz@users.noreply.github.com>
    Co-authored-by: erikawester <erikawester@users.noreply.github.com>
    Co-authored-by: brandonhv <brandonhv@users.noreply.github.com>

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SideBar from './SideBar.jsx'
+import Metrics from './Metrics.jsx'
+
+const Main = () => {
+
+    return (
+
+      <div>
+        <h1>Main page is connected</h1>
+        <SideBar/>
+        <Metrics/>
+      </div>
+
+    )
+  }
+
+  export default Main;
