6d797c578d577d8aa947ec53d5e445b0a7267af7
ppost0 <p.post2321@yahoo.com>
2024-01-31 19:08:15 -0500

    Change response object in middleware
    Edit response object in metricsController.
    
    Co-authored-by: Ines Chavez <Ineschvz@users.noreply.github.com>

--- server/controllers/metricsController.js
+++ server/controllers/metricsController.js
@@ -14,12 +14,40 @@ metricsController.getNodeList = async(req, res, next) => {
 
   try {
     const nodesList = await k8sApi.listNode();
-    res.locals.nodesList = nodesList;
-    console.log(nodesList.body, 1);
-    console.log(nodesList.body.items[0].status, 2);
-    response = await k8sApi.listPodForAllNamespaces();
-    console.log(response.body.items[0]);
-    // console.log('API Server URL:', kc.getCurrentCluster().server, 3);
+    console.log(nodesList.body.items[0]);
+
+    const nodesArr = nodesList.body.items;
+
+    let numOfNodes = nodesList.body.items.length;
+
+    let numOfPods;
+    let numOfDeployments;
+    let numOfNamespaces;
+
+
+
+
+    const axios = require('axios');
+
+    const metricsServerApiUrl = 'http://127.0.0.1:56110/apis/metrics.k8s.io/v1beta1/nodes';
+
+    const metricsRes = await axios.get(metricsServerApiUrl)
+    //https://127.0.0.1:56110
+
+    console.log(metricsRes);
+
+
+
+
+
+
+    res.locals.nodesList = {
+      numOfNodes,
+      numOfPods,
+      numOfDeployments,
+      numOfNamespaces
+    };
+
     return next();
   } catch (err) {
     console.error(err);
