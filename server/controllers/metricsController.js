d8cdcaef1c9140da82151616a40c341dff6ac953
Brandon <brandon5.9@hotmail.com>
2024-02-13 16:39:02 -0500

    Sending more detail data from the Nodes of the local cluster to the client (Currenly setting up constroller metricsController.getNodeList)

--- server/controllers/metricsController.js
+++ server/controllers/metricsController.js
@@ -16,56 +16,113 @@ const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 //Initial middleware object
 const metricsController = {};
 
-//adding method to metricsController
+// // adding method to metricsController
+// metricsController.getNodeList = async(req, res, next) => {
+
+//   try {
+
+//     //.listNode is an API client method (native built in method)
+//     const nodesList = await k8sApi.listNode();
+//     // console.log('NodeList -------->', nodesList);
+
+//     // const nodesArr = nodesList.body.items;
+
+//     let numOfNodes = nodesList.body.items.length;
+
+
+//     // TODO: Keep playing with the nodesList.body.items and/or nodeList....
+//     // To get importan information from each node like
+//       // UID
+//       // Namespace
+//     console.log('numOfNodes.body.items -------------------->', nodesList.body.items[0])
+
+//     res.locals.nodesList = {
+//       numOfNodes
+//     };
+
+//     return next();
+
+//     // ** TODO **
+//     //for each node: 
+//     //CPU/Memory:
+//     //Name:
+//     //Role:
+//     //Message throughput:
+//     //number of deployment:
+//     //number of pods:
+
+//     //find method for accessing number of pods
+//     // let numOfPods;
+//     //find method for accessing number of deployments
+//     // let numOfDeployments;
+
+
+//     // const test = await k8s.topNodes(k8sApi);
+//     // console.log(test, 'test');
+
+//     // const metricsClient = new k8s.Metrics(kc)
+//     // const nodeMetrics = await metricsClient.getNodeMetrics();
+//     // console.log(nodeMetrics, 'node metrics');
+
+    
+//   } catch (err) {
+//     console.error(err);
+//   }
+
+// }
+
+
+
 metricsController.getNodeList = async(req, res, next) => {
 
   try {
-    //.listNode is an API client method (native built in method)
+
     const nodesList = await k8sApi.listNode();
-    // console.log(nodesList.body.items[0]);
 
-    const nodesArr = nodesList.body.items;
 
-    let numOfNodes = nodesList.body.items.length;
-
-    //for each node: 
+    // ** TODO (For each Node)**
     //CPU/Memory:
     //Name:
     //Role:
     //Message throughput:
     //number of deployment:
-    //number of pods:
-
-
-    //find method for accessing number of pods
-    let numOfPods;
-    //find method for accessing number of deployments
-    let numOfDeployments;
-
-
-  
-
-    const listNode = await k8sApi.listNode(); 
-
-    const test = await k8s.topNodes(k8sApi);
-    console.log(test, 'test');
-
-    const metricsClient = new k8s.Metrics(kc)
-    const nodeMetrics = await metricsClient.getNodeMetrics();
-    console.log(nodeMetrics, 'node metrics');
+    //number of pods:lis
 
     res.locals.nodesList = {
-      numOfNodes,
-      numOfPods,
-      numOfDeployments,
+
+      totalNumberOfNodes: nodesList.body.items.length,
+      masterNode: {
+      name: nodesList.body.items[0].metadata.name,
+      creationTime: nodesList.body.items[0].metadata.creationTimestamp,
+      UID: nodesList.body.items[0].metadata.uid,
+      memoryCapacity: 'n/a',
+      CPU: 'n/a',
+      role: 'n/a'
+      },
+      workerNode1: {
+        name: nodesList.body.items[1].metadata.name,
+        creationTime: nodesList.body.items[1].metadata.creationTimestamp,
+        UID: nodesList.body.items[1].metadata.uid
+      },
+      workerNode2: {
+        name: nodesList.body.items[2].metadata.name,
+        creationTime: nodesList.body.items[2].metadata.creationTimestamp,
+        UID: nodesList.body.items[2].metadata.uid
+      },
+      workerNode3: {
+        name: nodesList.body.items[3].metadata.name,
+        creationTime: nodesList.body.items[3].metadata.creationTimestamp,
+        UID: nodesList.body.items[3].metadata.uid
+      }
     };
 
     return next();
+
   } catch (err) {
     console.error(err);
-  }
+  };
 
-}
+};
 
 
 
