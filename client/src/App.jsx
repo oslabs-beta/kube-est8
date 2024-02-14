c5e2c29a6cbd1aadda1c3f4f41cf233c2d096acc
erikawester <erikarwester@gmail.com>
2024-02-13 20:02:45 -0500

    Edit App.jsx to adapt to react-router-dom and new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import NavBar from './components/NavBar.jsx';
-import Main from './components/Main.jsx';
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom"; 
+import { AuthContextProvider } from "./context/AuthContent.js";
+import NavBar from "./components/NavBar.jsx";
+import Main from "./components/Main.jsx";
+import Signin from './components/Signin.jsx';
+
 
 const App = () => {
-
   return (
+    <div className="root">
+      <AuthContextProvider>
+      <NavBar/>
+        {/* <Router> */}
+           <Routes>
 
-    <div className='root'>
-      {/* <NavBar/> */}
-      <Main/>
+        <Route path='/' element={<Main/>} />
+        <Route path='/signin' element={<Signin/>}/>
+          </Routes>
+        {/* </Router> */}
+      </AuthContextProvider>
     </div>
-
-  )
-}
+  );
+};
 
 export default App;
