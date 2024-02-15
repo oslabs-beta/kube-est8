d3de5d9aef2b51263b399986bde1ff4943b087c0
ppost0 <p.post2321@yahoo.com>
2024-02-15 17:40:15 -0500

    Change styling, layout of cluster overview, removed some images

--- client/src/components/MetricsTable.jsx
+++ client/src/components/MetricsTable.jsx
@@ -10,13 +10,13 @@ const MetricsTable = (props) => {
       return (
 
         <div className='MetricsTable'>
-          <h2 className='dataTitle' >Cluster Overview</h2>
-          <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=1" width="1200" height="200" frameborder="0"></iframe>
+          {/* <h2 className='dataTitle' >Cluster Overview</h2> */}
+          <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=1"  frameborder="0"></iframe>
           <div className='grafana1'>
-            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=2" width="600" height="200" frameborder="0"></iframe>
-            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=3" width="600" height="200" frameborder="0"></iframe>
-            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=4" width="600" height="200" frameborder="0"></iframe>
-            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=5" width="600" height="200" frameborder="0"></iframe>
+            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=2"  frameborder="0"></iframe>
+            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=3"  frameborder="0"></iframe>
+            <iframe className='grafanaGraph' src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=4" height="500" frameborder="0"></iframe>
+            <iframe src="http://localhost:3000/d-solo/b3e4168a-0a31-445c-8ec2-2f635a2b285a/test-2?orgId=1&refresh=5s&panelId=5" className='grafanaGraph' height="500" frameborder="0"></iframe>
           </div>
         </div>
 
