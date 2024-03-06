c8546bf5dd7be7dfefac12be36058de3392c05c6
ppost0 <p.post2321@yahoo.com>
2024-03-06 13:38:36 -0500

    Setup boilerplate in controller for throughput

--- server/controllers/throughputController.ts
+++ server/controllers/throughputController.ts
@@ -1,2 +1,30 @@
-import { Request, Response, NextFunction } from 'express';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
 
+const throughputController = {
+
+
+  getThroughput: async (req, res, next) => {
+
+    try {
+
+
+
+
+
+
+
+
+      return next();
+    }
+    catch {
+
+    }
+
+  }
+
+}
+
+
+
+
+export default throughputController;
